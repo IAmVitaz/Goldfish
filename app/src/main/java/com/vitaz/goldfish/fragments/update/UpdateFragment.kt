e1f967a95cbd4fc418fe092fc79e5a8364e2e737
vit_az <vit.azarov@gmail.com>
2020-10-22 06:49:21 -0300

    added ability to edit and save tasks. moved parsePriorityToInt to SharedViewModel to have access to it from everywhere

--- app/src/main/java/com/vitaz/goldfish/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/vitaz/goldfish/fragments/update/UpdateFragment.kt
@@ -2,13 +2,18 @@ package com.vitaz.goldfish.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.vitaz.goldfish.R
 import com.vitaz.goldfish.data.models.Priority
+import com.vitaz.goldfish.data.models.ToDoData
+import com.vitaz.goldfish.data.viewmodel.ToDoViewModel
 import com.vitaz.goldfish.fragments.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.android.synthetic.main.fragment_update.*
 import kotlinx.android.synthetic.main.fragment_update.view.*
 
 class UpdateFragment : Fragment() {
@@ -16,6 +21,7 @@ class UpdateFragment : Fragment() {
     private val args by navArgs<UpdateFragmentArgs>()
 
     private val mSharedViewModel: SharedViewModel by viewModels()
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +37,7 @@ class UpdateFragment : Fragment() {
         //receive object from list fragment and populate fields:
         view.current_title_et.setText(args.currentItem.title)
         view.current_description_et.setText(args.currentItem.description)
-        view.current_priority_spinner.setSelection(parsePriority(args.currentItem.priority))
+        view.current_priority_spinner.setSelection(mSharedViewModel.parsePriorityToInt(args.currentItem.priority))
         view.current_priority_spinner.onItemSelectedListener = mSharedViewModel.listener
 
         return view
@@ -42,12 +48,35 @@ class UpdateFragment : Fragment() {
         inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
-    private fun parsePriority(priority: Priority): Int {
-        return when(priority) {
-            Priority.HIGH -> 0
-            Priority.MEDIUM -> 1
-            Priority.LOW -> 2
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_save){
+            updateItem()
         }
+        return super.onOptionsItemSelected(item)
     }
 
+    private fun updateItem() {
+        val title = current_title_et.text.toString()
+        val description = current_description_et.text.toString()
+        val getPriority = current_priority_spinner.selectedItem.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(title, description)
+        if (validation) {
+            //Update current item
+            val updatedItem = ToDoData(
+                args.currentItem.id,
+                title,
+                mSharedViewModel.parsePriority(getPriority),
+                description
+            )
+            mToDoViewModel.updateData(updatedItem)
+            Toast.makeText(requireContext(), "Successfully updated!", Toast.LENGTH_LONG).show()
+            //Navigate back
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields!", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
 }
